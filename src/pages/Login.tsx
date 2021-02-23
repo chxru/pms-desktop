8e912192684840020ed1a90f68642cf2cd70b3f5
CharukaHS <charuka@protonmail.com>
2021-02-23 08:56:21 +0530

    Login function, with pouch-db

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
   Box,
   Button,
   Center,
   Container,
+  Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
@@ -11,6 +12,10 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { ipcRenderer } from 'electron';
+
+import AuthContext from '../context/auth-context';
 
 interface LoginForm {
   username: string;
@@ -18,18 +23,45 @@ interface LoginForm {
 }
 
 const LoginPage: React.FC = () => {
+  const auth = useContext(AuthContext);
+  const history = useHistory();
   const { handleSubmit, errors, register } = useForm<LoginForm>();
 
+  // listen to ipc
+  useEffect(() => {
+    ipcRenderer.on(
+      'check-uname-pwd-res',
+      (_, args: { res: boolean; error?: string }) => {
+        if (args.res) {
+          auth.SignIn();
+        }
+      }
+    );
+    return () => {
+      ipcRenderer.removeListener('check-uname-pwd-res', () => {});
+    };
+  }, [auth]);
+
+  // form submit
   const onSubmit = (values: LoginForm) => {
     // eslint-disable-next-line no-console
     console.log(values);
+    ipcRenderer.send('check-uname-pwd', values);
   };
 
   return (
     <Center w="100vw" h="100vh">
       <Container width="350px">
-        <Text>Login</Text>
-        <Box backgroundColor="white">
+        <Text align="center" paddingY="2" fontSize="lg">
+          Login
+        </Text>
+        <Box
+          backgroundColor="white"
+          paddingY="2"
+          paddingX="4"
+          boxShadow="lg"
+          rounded="lg"
+        >
           <form onSubmit={handleSubmit(onSubmit)}>
             <FormControl>
               <FormLabel htmlFor="username">Username</FormLabel>
@@ -52,7 +84,21 @@ const LoginPage: React.FC = () => {
               {errors.username && errors.username.message}
               {errors.password && errors.password.message}
             </FormErrorMessage>
-            <Button type="submit">Login</Button>
+
+            <Flex direction="column">
+              <Button type="submit" m="2" colorScheme="teal">
+                Login
+              </Button>
+              <Button
+                m="2"
+                size="sm"
+                onClick={() => {
+                  history.push('/register');
+                }}
+              >
+                Create an account
+              </Button>
+            </Flex>
           </form>
         </Box>
       </Container>
---
+++ src/pages/Register.tsx
@@ -0,0 +1,106 @@
+import React, { useContext, useEffect } from 'react';
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Text,
+} from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { ipcRenderer } from 'electron';
+
+import AuthContext from '../context/auth-context';
+
+interface RegisterForm {
+  username: string;
+  password: string;
+}
+
+const RegisterPage: React.FC = () => {
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+  const { handleSubmit, errors, register } = useForm<RegisterForm>();
+
+  // listen to ipc
+  useEffect(() => {
+    ipcRenderer.on(
+      'register-new-user-res',
+      (_, args: { res: boolean; error?: string }) => {
+        if (args.res) {
+          auth.SignIn();
+        }
+      }
+    );
+    return () => {
+      ipcRenderer.removeListener('register-new-user-res', () => {});
+    };
+  }, [auth]);
+
+  const onSubmit = (values: RegisterForm) => {
+    ipcRenderer.send('register-new-user', values);
+  };
+
+  return (
+    <Center w="100vw" h="100vh">
+      <Container width="350px">
+        <Text align="center" paddingY="2" fontSize="lg">
+          Register
+        </Text>
+        <Box
+          backgroundColor="white"
+          paddingY="2"
+          paddingX="4"
+          boxShadow="lg"
+          rounded="lg"
+        >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                name="username"
+                placeholder="username"
+                ref={register({ required: true })}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                name="password"
+                placeholder="password"
+                type="password"
+                ref={register({ required: true })}
+              />
+            </FormControl>
+            <FormErrorMessage>
+              {errors.username && errors.username.message}
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+
+            <Flex direction="column">
+              <Button type="submit" m="2" colorScheme="teal">
+                Register
+              </Button>
+              <Button
+                m="2"
+                size="sm"
+                onClick={() => {
+                  history.push('/login');
+                }}
+              >
+                Already have an account?
+              </Button>
+            </Flex>
+          </form>
+        </Box>
+      </Container>
+    </Center>
+  );
+};
+
+export default RegisterPage;
